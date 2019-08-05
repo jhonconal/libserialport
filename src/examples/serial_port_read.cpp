624ff6393e34d2e9c2527601f0ff6ff289d49b10
kevin.yang <jhonconal@outlook.com>
2019-08-05 18:12:05 +0800

    [init]    Frist init.

---
+++ src/examples/serial_port_read.cpp
@@ -0,0 +1,100 @@
+/**
+ *  @example serial_port_read.cpp
+ */
+
+#include "../libserial/SerialPort.h"
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+
+constexpr const char* const SERIAL_PORT_1 = "/dev/ttyUSB0" ;
+
+/**
+ * @brief This example demonstrates configuring a serial port and 
+ *        reading serial data.
+ */
+int main()
+{
+    using namespace LibSerial ;
+
+    // Instantiate a SerialPort object.
+    SerialPort serial_port ;
+
+    try
+    {
+        // Open the Serial Port at the desired hardware port.
+        serial_port.Open(SERIAL_PORT_1) ;
+    }
+    catch (const OpenFailed&)
+    {
+        std::cerr << "The serial port did not open correctly." << std::endl ;
+        return EXIT_FAILURE ;
+    }
+
+    // Set the baud rate of the serial port.
+    serial_port.SetBaudRate(BaudRate::BAUD_115200) ;
+
+    // Set the number of data bits.
+    serial_port.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
+
+    // Turn off hardware flow control.
+    serial_port.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
+
+    // Disable parity.
+    serial_port.SetParity(Parity::PARITY_NONE) ;
+    
+    // Set the number of stop bits.
+    serial_port.SetStopBits(StopBits::STOP_BITS_1) ;
+    
+    // Wait for data to be available at the serial port.
+    while(!serial_port.IsDataAvailable()) 
+    {
+        usleep(1000) ;
+    }
+
+    // Specify a timeout value (in milliseconds).
+    size_t ms_timeout = 250 ;
+
+    // Char variable to store data coming from the serial port.
+    char data_byte ;
+
+    // Read one byte from the serial port and print it to the terminal.
+    try
+    {
+        // Read a single byte of data from the serial port.
+        serial_port.ReadByte(data_byte, ms_timeout) ;
+
+        // Show the user what is being read from the serial port.
+        std::cout << data_byte << std::flush ;
+    }
+    catch (const ReadTimeout&)
+    {
+        std::cerr << "\nThe ReadByte() call has timed out." << std::endl ;
+    }
+
+    // Wait a brief period for more data to arrive.
+    usleep(1000) ;
+
+    DataBuffer read_buffer ;
+
+    try
+    {
+        // Read as many bytes as are available during the timeout period.
+        serial_port.Read(read_buffer, 0, ms_timeout) ;
+    }
+    catch (const ReadTimeout&)
+    {
+        for (size_t i = 0 ; i < read_buffer.size() ; i++)
+        {
+            std::cout << read_buffer.at(i) << std::flush ;
+        }
+
+        std::cerr << "The Read() call timed out waiting for additional data." << std::endl ;
+    }
+
+    // Successful program completion.
+    std::cout << "The example program successfully completed!" << std::endl ;
+    return EXIT_SUCCESS ;
+}
---
+++ src/examples/serial_stream_read.cpp
@@ -0,0 +1,75 @@
+/**
+ *  @example serial_stream_read.cpp
+ */
+
+#include "../libserial/SerialPort.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+
+constexpr const char* const SERIAL_PORT_1 = "/dev/ttyUSB0" ;
+
+/**
+ * @brief This example demonstrates configuring a serial stream and 
+ *        reading serial stream data.
+ */
+int main()
+{
+    using namespace LibSerial ;
+
+    // Instantiate a SerialStream object.
+    SerialStream serial_stream ;
+
+    try
+    {
+        // Open the Serial Port at the desired hardware port.
+        serial_stream.Open(SERIAL_PORT_1) ;
+    }
+    catch (const OpenFailed&)
+    {
+        std::cerr << "The serial port did not open correctly." << std::endl ;
+        return EXIT_FAILURE ;
+    }
+
+    // Set the baud rate of the serial port.
+    serial_stream.SetBaudRate(BaudRate::BAUD_115200) ;
+
+    // Set the number of data bits.
+    serial_stream.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
+
+    // Turn off hardware flow control.
+    serial_stream.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
+
+    // Disable parity.
+    serial_stream.SetParity(Parity::PARITY_NONE) ;
+
+    // Set the number of stop bits.
+    serial_stream.SetStopBits(StopBits::STOP_BITS_1) ;
+
+    // Wait for data to be available at the serial port.
+    while(serial_stream.rdbuf()->in_avail() == 0) 
+    {
+        usleep(1000) ;
+    }
+
+    // Keep reading data from serial port and print it to the screen.
+    while(serial_stream.IsDataAvailable()) 
+    {
+        // Variable to store data coming from the serial port.
+        char data_byte ;
+
+        // Read a single byte of data from the serial port.
+        serial_stream.get(data_byte) ;
+
+        // Show the user what is being read from the serial port.
+        std::cout << data_byte ;
+
+        // Wait a brief period for more data to arrive.
+        usleep(1000) ;
+    }
+
+    // Successful program completion.
+    std::cout << "Done." << std::endl ;
+    return EXIT_SUCCESS ;
+}
